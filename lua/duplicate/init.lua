b74066596a37e80cae724c97f6f50a79c122b0b8
smjonas <jonas.strittmatter@gmx.de>
2023-01-18 15:08:27 +0100

    Initial implementation

---
+++ lua/duplicate/init.lua
@@ -0,0 +1,42 @@
+local Duplicate = {}
+
+Duplicate.operator = function(mode)
+  if mode == nil then
+    vim.cmd("set operatorfunc=v:lua.Duplicate.operator")
+    return "g@"
+  end
+
+  local mark_left, mark_right = "[", "]"
+  local mark_left, mark_right = "[", "]"
+  if mode == "visual" then
+    mark_left, mark_right = "<", ">"
+  end
+
+  local line_left, col_left = unpack(vim.api.nvim_buf_get_mark(0, mark_left))
+  local line_right, col_right = unpack(vim.api.nvim_buf_get_mark(0, mark_right))
+
+  -- Do nothing
+  if (line_left > line_right) or (line_left == line_right and col_left > col_right) then
+    return
+  end
+
+  -- Using `vim.cmd()` wrapper to allow usage of `lockmarks` command, because
+  -- raw execution will delete marks inside region (due to
+  -- `vim.api.nvim_buf_set_lines()`).
+  vim.cmd(string.format("lockmarks lua Duplicate.duplicate_lines(%d, %d)", line_left, line_right))
+  return ""
+end
+
+Duplicate.duplicate_lines = function(line_start, line_end)
+  local lines = vim.api.nvim_buf_get_lines(0, line_start - 1, line_end, false)
+  vim.api.nvim_buf_set_lines(0, line_end, line_end, false, lines)
+end
+
+Duplicate.setup = function()
+  -- Export module
+  _G.Duplicate = Duplicate
+  vim.keymap.set("n", "dp", "v:lua.Duplicate.operator()", { expr = true, desc = "Duplicate" })
+  vim.keymap.set("x", "dp", ":<c-u>lua Duplicate.operator('visual')<cr>", { desc = "Duplicate" })
+end
+
+return Duplicate
